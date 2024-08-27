65bd7fcbeeb80f6d6b9c15b6005a0d89084c0ddf
ronste <ronald.steffen@fu-berlin.de>
2024-08-27 10:23:26 +0000

    fix issue #25

--- filter/DNBXmlFilter.inc.php
+++ filter/DNBXmlFilter.inc.php
@@ -125,7 +125,7 @@ class DNBXmlFilter extends NativeExportFilter {
 			if ($issue->getAccessStatus() == 0 || $issue->getAccessStatus() == ISSUE_ACCESS_OPEN) {
 				$openAccess = true;
 			} else if ($issue->getAccessStatus() == ISSUE_ACCESS_SUBSCRIPTION) {
-				if ($submission->getAccessStatus() == ARTICLE_ACCESS_OPEN) {
+				if ($submission->getCurrentPublication()->getData('accessStatus') == ARTICLE_ACCESS_OPEN) {
 					$openAccess = true;
 				}
 			}
